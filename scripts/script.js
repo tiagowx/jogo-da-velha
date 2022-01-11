64d457a7a11de0de1abc39ab3cb84317b122bf0c
wxtiago <tiagobatista.tec@gmail.com>
2022-01-11 19:57:09 -0300

    Parte 5

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,33 @@
 var player = null;
-
-var selectedPlayer = document.getElementById('player-selected')
+var selectedPlayer = document.getElementById('player-selected');
+var squares = document.getElementsByClassName('square');
 
 changePlayer('X')
 
 function selectSquare(id) {
     var square = document.getElementById(id);
+
+    if (square.innerHTML !== '-')
+        return;
+
     square.innerHTML = player;
     square.style.color = '#000';
+
+    if (player === 'X')
+        player = 'O';
+    else
+        player = 'X';
 }
 
 function changePlayer(value) {
     player = value;
     selectedPlayer.innerHTML = player;
+}
+
+function checkWin() {
+
+}
+
+function checkSequence() {
+
 }
